728157ea32b5bd34ccceda95d20d3b6b3a9ebffb
doki_pen <doki_pen@doki-pen.org>
2010-01-19 13:55:36 -0500

    made slim macro, switched button macro to button widget

--- tracxspf/macros.py
+++ tracxspf/macros.py
@@ -6,20 +6,37 @@ from trac.wiki.api import parse_args
 from trac.wiki.macros import WikiMacroBase
 from trac.util.html import escape
 
-class XspfButtonMacro(WikiMacroBase):
+class XspfSlimMacro(WikiMacroBase):
     def expand_macro(self, formatter, name, content):
         _, kw = parse_args(content)
-        song_url = "/raw-attachment/wiki"\
-                "/%s/%s"%(formatter.resource.id, kw.get('attachment'))
         name = kw.get('name', 'Unknown')
         artist = kw.get('artist', 'Unknown')
         song_title = escape("%s - %s"%(name, artist))
+        wiki = kw.get('wiki', formatter.resource.id)
+        attachment = kw.get('attachment')
+        song_url = "/raw-attachment/wiki/%s/%s"%(wiki, attachment)
         data = Chrome(self.env).populate_data(formatter.req, {
             'song_url': song_url,
             'song_title': song_title,
             'name': name,
             'artist': artist
         });
-        return Chrome(self.env).load_template('xspf.html').\
+        return Chrome(self.env).load_template('xspf-slim.html').\
+                generate(**data)
+
+class XspfButtonMacro(WikiMacroBase):
+    def expand_macro(self, formatter, name, content):
+        _, kw = parse_args(content)
+        name = kw.get('name', 'Unknown')
+        wiki = kw.get('wiki', formatter.resource.id)
+        artist = kw.get('artist', 'Unknown')
+        attachment = kw.get('attachment')
+        song_url = "/raw-attachment/wiki/%s/%s"%(wiki, attachment)
+        data = Chrome(self.env).populate_data(formatter.req, {
+            'song_url': song_url,
+            'name': name,
+            'artist': artist
+        });
+        return Chrome(self.env).load_template('xspf-button.html').\
                 generate(**data)
 
